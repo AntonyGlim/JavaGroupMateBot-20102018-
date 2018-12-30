2b7802fd43d71e7c5b7ba0b000b871f76a867cf0
AntonyGlim <Antony.Glim@gmail.com>
2018-12-30 14:31:15 +0300

    Добавлена инициализация бота, его создание и регистрация

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -1,4 +1,17 @@
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.TelegramBotsApi;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.exceptions.TelegramApiException;
 
 public class Bot extends TelegramLongPollingBot {
+    public static void main(String[] args) {
+        ApiContextInitializer.init();                                           //Инициализируем API
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi();                //Создаем объект
+        //Блок регистрации бота
+        try {
+            telegramBotsApi.registerBot(new Bot());
+        } catch (TelegramApiException e){
+            e.printStackTrace();
+        }
+    }
 }
